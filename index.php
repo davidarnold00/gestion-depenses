32445ef58b0766cb6d34a2e8faa077360d356459
davidarnold00 <davidarnoldgnakale@gmail.com>
2026-03-18 03:47:53 +0100

    recuperation des depenses pour le tableau

--- index.php
+++ index.php
@@ -9,17 +9,3 @@ include 'db.php';
 /** 1. RÉCUPÉRATION DES DONNÉES POUR LE TABLEAU **/
 $result = $conn->query("SELECT * FROM depenses ORDER BY date_depense DESC");
 
-/** 2. CALCUL DU TOTAL GÉNÉRAL **/
-$total_query = $conn->query("SELECT SUM(Montant) as total FROM depenses");
-$total_general = $total_query->fetch_assoc()['total'] ?? 0;
-
-/** 3. RÉCUPÉRATION DES STATS POUR LA LISTE ET LE GRAPHIQUE **/
-// Attention : 'categorie' en minuscule ici selon ta DB
-$sql_stats = "SELECT categorie, SUM(Montant) as somme FROM depenses GROUP BY categorie";
-$stats_result = $conn->query($sql_stats);
-
-$categories = [];
-$montants = [];
-$stats_rows = [];
-
-g
