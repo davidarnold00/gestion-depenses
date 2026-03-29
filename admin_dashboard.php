0f178f05fce5440085395df083112b2aecf34ffa
davidarnold00 <davidarnoldgnakale@gmail.com>
2026-03-30 01:37:21 +0200

    calcul de la somme des depneses de tous les utilasteurs pour mon admin

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -5,3 +5,6 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
     header("Location: index.php"); // Redirige les simples utilisateurs
     exit();
 }
+// je calcule la somme de TOUTES les dépenses de TOUS les utilisateurs dans ma db
+$total_res = $conn->query("SELECT SUM(montant) as total_global FROM depenses");
+$total_global = $total_res->fetch_assoc()['total_global'];
