409827ed5ddcbf9106cd004297590beb5b3de87f
davidarnold00 <davidarnoldgnakale@gmail.com>
2026-03-30 01:39:53 +0200

    liste des utilisateur et leur nombre de depense respectif

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -8,3 +8,7 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
 // je calcule la somme de TOUTES les dépenses de TOUS les utilisateurs dans ma db
 $total_res = $conn->query("SELECT SUM(montant) as total_global FROM depenses");
 $total_global = $total_res->fetch_assoc()['total_global'];
+
+// J'ai fait une jointure pour lister les utilisateurs et leur nombre de dépenses respectif
+$users_res = $conn->query("SELECT u.id, u.login, u.role, COUNT(d.id) as nb_depenses FROM utilisateurs u LEFT JOIN depenses d ON u.id = d.user_id GROUP BY u.id");
+?>
