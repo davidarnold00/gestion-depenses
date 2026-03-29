761ba170c9155c7f4479a29d558e66774e0964b2
davidarnold00 <davidarnoldgnakale@gmail.com>
2026-03-30 01:46:05 +0200

    condition de connexion pour interroger lapi

--- api_stats.php
+++ api_stats.php
@@ -4,3 +4,9 @@ header('Content-Type: application/json');
 require_once 'db.php';
 session_start();
 
+// Sécurité : Seul un utilisateur connecté peut interroger mon API
+if (!isset($_SESSION['user_id'])) {
+    echo json_encode(["error" => "Accès non autorisé"]);
+    exit();
+}
+
